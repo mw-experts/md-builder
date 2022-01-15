33ff759a5ecf065c0d62695d8068ddd900c77ce0
Andrey Korovin <misticwonder@gmail.com>
2022-01-15 15:26:47 +0200

    create lib md-builder

---
+++ packages/md-builder/src/lib/md-builder.spec.ts
@@ -0,0 +1,7 @@
+import { mdBuilder } from './md-builder';
+
+describe('mdBuilder', () => {
+  it('should work', () => {
+    expect(mdBuilder()).toEqual('md-builder');
+  });
+});
---
+++ packages/md-builder/src/lib/md-builder.ts
@@ -0,0 +1,3 @@
+export function mdBuilder(): string {
+  return 'md-builder';
+}
