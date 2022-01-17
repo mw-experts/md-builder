365e81c9c38b4d5074201d6c5c23c53c6fdda1d9
Andrey Korovin <misticwonder@gmail.com>
2022-01-17 19:43:28 +0200

    feat(md-builder): spawn binary approach

--- packages/md-builder/src/index.ts
+++ packages/md-builder/src/index.ts
@@ -1,1 +1,21 @@
-export * from './lib/md-builder';
+import { ChildProcessWithoutNullStreams, spawn } from 'node:child_process';
+
+const child: ChildProcessWithoutNullStreams = spawn('../mdbook-mac', process.argv.slice(2));
+
+child.stdout.on('data', (data: unknown): void => {
+  // eslint-disable-next-line no-console
+  console.log(`stdout:\n${data}`);
+});
+
+child.stderr.on('data', (data: unknown): void => {
+  console.error(`stderr: ${data}`);
+});
+
+child.on('error', (error: Error): void => {
+  console.error(`error: ${error.message}`);
+});
+
+child.on('close', (code: number): void => {
+  // eslint-disable-next-line no-console
+  console.log(`child process exited with code ${code}`);
+});
--- packages/md-builder/src/lib/md-builder.ts
+++
@@ -1,3 +0,0 @@
-export function mdBuilder(): string {
-  return 'md-builder';
-}
